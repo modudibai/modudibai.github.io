2beb701c121adce9249a099a4395118332d3b0fa
modudibai <113973285+modudibai@users.noreply.github.com>
2023-03-10 17:24:07 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+return {
+  sgs: {
+    entIPA: 'https://tmp-azeroth.vx-cdn.com/file-63f8e2b37505d-6409496466efc/sgsmjz-ent-0224.ipa',
+    vipIPA: 'https://tmp-azeroth.vx-cdn.com/file-64006f425291c-64094971adad6/sgsmjz-vip-0302.ipa',
+    entId: 'com.HKSoft.JinfengEnergApp.1',
+    vipId: 'com.sanguosha.mjzoo',
+    version: '4.2.7',
+    title: '三国名将传+',
+    image: 'https://img11.18183.com/ku18183/image/2020/04/09/d82ea75be4235867aa63a27c550afb3f.jpg'
+  }
+}
