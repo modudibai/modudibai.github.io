46d1f9ff66e95f8f5bde1462f0bde66c31ba17d9
modudibai <113973285+modudibai@users.noreply.github.com>
2023-03-10 17:33:27 +0800

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,15 @@ var configData = {
     title: '三国名将传+',
     image: 'https://img11.18183.com/ku18183/image/2020/04/09/d82ea75be4235867aa63a27c550afb3f.jpg'
   },
+  6kw: {
+    entIPA: 'https://tmp-azeroth.vx-cdn.com/file-6404b1ddd82fa-6407fed44d0a2/sgyxl-ent-0305b.ipa',
+    vipIPA: 'https://tmp-azeroth.vx-cdn.com/file-6404b1ddd82fa-6407fed44d0a2/sgyxl-ent-0305b.ipa',
+    entId: 'com.HKSoft.JinfengEnergApp.1',
+    vipId: 'com.dibai.sgyxl',
+    version: '5.1.11',
+    title: '三国英雄录+',
+    image: 'https://ios.6kw.com/sgyxl1/57.png'
+  },
   zzxy: {
     entIPA: 'https://tmp-azeroth.vx-cdn.com/file-64031df6dbede-64074df22cc4b/zzxy-ent-0304.ipa',
     vipIPA: 'https://tmp-azeroth.vx-cdn.com/file-64031df6dbede-64074df22cc4b/zzxy-ent-0304.ipa',
