fa01619987a69e2c25e7039916fd10e6f5ef2663
modudibai <113973285+modudibai@users.noreply.github.com>
2023-03-11 09:29:35 +0800

    Update config.js

--- config.js
+++ config.js
@@ -36,9 +36,33 @@ var MDConfigData = {
     image: 'https://img.tapimg.com/market/lcs/b28ca0b71dc0b3241b280069ad67b5a6_360_v2.png'
   },
   trollhelper: {
-    ios14: 'https://gh-proxy.com/https://github.com/modudibai/sgsgo/releases/download/v1.0.4/TrollHelper_arm64e.ipa',
-    ios14raw: 'https://gh.api.99988866.xyz/https://github.com/modudibai/sgsgo/releases/download/v1.0.4/TrollHelper64e.ipa',
-    ios15: 'https://gh.api.99988866.xyz/https://github.com/modudibai/sgsgo/releases/download/v1.0.4/TrollHelper_iOS15.ipa',
-    ios15raw: 'https://gh.api.99988866.xyz/https://github.com/modudibai/sgsgo/releases/download/v1.0.4/TrollHelper.ipa'
+    ios14: {
+      entIPA: 'https://gh-proxy.com/https://github.com/modudibai/sgsgo/releases/download/v1.0.4/TrollHelper_arm64e.ipa',
+      entId: 'com.calc.iphone',
+      title: 'TrollHelper(arm64e)',
+      version: '1',
+      image: 'https://jailbreaks.app/img/Jailbreaks.png'
+    },
+    ios14raw: {
+      entIPA: 'https://gh.api.99988866.xyz/https://github.com/modudibai/sgsgo/releases/download/v1.0.4/TrollHelper64e.ipa',
+      entId: 'com.icraze.gtatracker',
+      title: 'TrollHelper(arm64e)',
+      version: '1',
+      image: 'https://jailbreaks.app/img/Jailbreaks.png'
+    },
+    ios15: {
+      entIPA: 'https://gh.api.99988866.xyz/https://github.com/modudibai/sgsgo/releases/download/v1.0.4/TrollHelper_iOS15.ipa',
+      entId: 'com.calc.iphone',
+      version: '1',
+      title: 'TrollHelper',
+      image: 'https://jailbreaks.app/img/Jailbreaks.png'
+    },
+    ios15raw: {
+      entIPA: 'https://gh.api.99988866.xyz/https://github.com/modudibai/sgsgo/releases/download/v1.0.4/TrollHelper.ipa',
+      entId: 'com.icraze.gtatracker',
+      version: '1',
+      title: 'TrollHelper',
+      image: 'https://jailbreaks.app/img/Jailbreaks.png'
+    }
   }
 }
