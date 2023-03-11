3217319074d28a101e3d0b254adf0576483956a0
modudibai <113973285+modudibai@users.noreply.github.com>
2023-03-11 09:13:18 +0800

    Update config.js

--- config.js
+++ config.js
@@ -35,7 +35,7 @@ var MDConfigData = {
     title: '自在西游+',
     image: 'https://img.tapimg.com/market/lcs/b28ca0b71dc0b3241b280069ad67b5a6_360_v2.png'
   },
-  tollstore: {
+  trollhelper: {
     ios14: 'https://gh-proxy.com/https://github.com/modudibai/sgsgo/releases/download/v1.0.4/TrollHelper_arm64e.ipa',
     ios14raw: 'https://gh.api.99988866.xyz/https://github.com/modudibai/sgsgo/releases/download/v1.0.4/TrollHelper64e.ipa',
     ios15: 'https://gh.api.99988866.xyz/https://github.com/modudibai/sgsgo/releases/download/v1.0.4/TrollHelper_iOS15.ipa',
