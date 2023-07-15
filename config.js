cb97b30f498034b63a2f7cd092b331cfb346b852
modudibai <113973285+modudibai@users.noreply.github.com>
2023-07-15 22:25:54 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var MDConfigData = {
   sgs: {
-    entIPA: 'https://tmp-titan.vx-cdn.com/file-64b10016cfda3-64b2981364aa8/sgs-ent-0714.ipa',
+    entIPA: 'https://tmp-titan.vx-cdn.com/file-64b10016cfda3-64b2ac43c3815/sgs-ent-0714.ipa',
     vipIPA: 'https://tmp-titan.vx-cdn.com/file-64ab7dc6820c3-64b27f18632d6/sgs-vip-0710.ipa',
     entId: 'com.sany.trucklinkcustomer',
     vipId: 'com.sanguosha.mjzoo',
@@ -18,8 +18,8 @@ var MDConfigData = {
     image: 'https://img11.18183.com/ku18183/image/2020/04/09/d82ea75be4235867aa63a27c550afb3f.jpg'
   },
   sgyxl: {
-    entIPA: 'https://tmp-titan.vx-cdn.com/file-64b201c649336-64b27f1d29a54/yxl-ent-0714.ipa',
-    vipIPA: 'https://tmp-titan.vx-cdn.com/file-64b201c649336-64b27f1d29a54/yxl-ent-0714.ipa',
+    entIPA: 'https://tmp-titan.vx-cdn.com/file-64b201c649336-64b2ac46c6042/yxl-ent-0714.ipa',
+    vipIPA: 'https://tmp-titan.vx-cdn.com/file-64b201c649336-64b2ac46c6042/yxl-ent-0714.ipa',
     entId: 'com.sany.trucklinkcustomer',
     vipId: 'com.dibai.sgyxl',
     version: '5.1.11',
@@ -36,8 +36,8 @@ var MDConfigData = {
     image: 'https://img11.18183.com/ku18183/image/2020/04/09/d82ea75be4235867aa63a27c550afb3f.jpg'
   },
   zzxy: {
-    entIPA: 'https://tmp-titan.vx-cdn.com/file-64b2034841262-64b27f20457ec/zzxy-ent-0714.ipa',
-    vipIPA: 'https://tmp-titan.vx-cdn.com/file-64b2034841262-64b27f20457ec/zzxy-ent-0714.ipa',
+    entIPA: 'https://tmp-titan.vx-cdn.com/file-64b2034841262-64b2ac46c7efc/zzxy-ent-0714.ipa',
+    vipIPA: 'https://tmp-titan.vx-cdn.com/file-64b2034841262-64b2ac46c7efc/zzxy-ent-0714.ipa',
     entId: 'com.sany.trucklinkcustomer',
     vipId: 'com.dibai.zzxy',
     version: '1.1.31',
