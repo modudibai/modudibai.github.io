6d777f424ed11aaabcb16c6550372a396ba97840
modudibai <113973285+modudibai@users.noreply.github.com>
2023-08-02 17:52:03 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var MDConfigData = {
   sgs: {
     entIPA: 'https://tmp-titan.vx-cdn.com/file-64c489f8699b9-64ca1dc979733/sgs-ent-0729.ipa',
     vipIPA: 'https://tmp-titan.vx-cdn.com/file-64bdcf24f191f-64c86f70bb670/sgs-vip-0724.ipa',
-    entId: 'com.painzisima.nmsl',
+    entId: 'com.reci.scan',
     vipId: 'com.sanguosha.mjzoo',
     version: '4.2.7',
     title: '三国名将传+',
@@ -11,7 +11,7 @@ var MDConfigData = {
   sgs2: {
     entIPA: 'https://ghproxy.com/https://github.com/modudibai/sgsgo/releases/download/v1.0.5/sgs-ent-0714.ipa',
     vipIPA: 'https://tmp-titan.vx-cdn.com/file-64bdcf24f191f-64be6aeb8f2e3/sgs-vip-0724.ipa',
-    entId: 'com.painzisima.nmsl',
+    entId: 'com.reci.scan',
     vipId: 'com.sanguosha.mjzoo',
     version: '4.2.7',
     title: '三国名将传+',
@@ -20,7 +20,7 @@ var MDConfigData = {
   sgyxl: {
     entIPA: 'https://tmp-titan.vx-cdn.com/file-64bbafeebd880-64c1cbee97975/yxl-ent-0722.ipa',
     vipIPA: 'https://tmp-titan.vx-cdn.com/file-64c9ee5035d44-64c9ee7a95d90/yxl-0802.ipa',
-    entId: 'com.painzisima.nmsl',
+    entId: 'com.reci.scan',
     vipId: 'com.dibai.sgyxl',
     version: '5.1.11',
     title: '三国英雄录+',
@@ -29,7 +29,7 @@ var MDConfigData = {
   sgz: {
     entIPA: 'https://tmp-titan.vx-cdn.com/file-64ca2126d5c9f-64ca24f5c0b2a/sgz-0802.ipa',
     vipIPA: 'https://tmp-titan.vx-cdn.com/file-64ca2126d5c9f-64ca24f5c0b2a/sgz-0802.ipa',
-    entId: 'com.painzisima.nmsl',
+    entId: 'com.reci.scan',
     vipId: 'com.dibai.sgzmjz',
     version: '5.1.11',
     title: '三国志名将+',
@@ -38,7 +38,7 @@ var MDConfigData = {
   zzxy: {
     entIPA: 'https://tmp-titan.vx-cdn.com/file-64bbad124d3e7-64c1cbef8b918/zzxy-ent-0722.ipa',
     vipIPA: 'https://tmp-titan.vx-cdn.com/file-64bbad124d3e7-64c1cbef8b918/zzxy-ent-0722.ipa',
-    entId: 'com.painzisima.nmsl',
+    entId: 'com.reci.scan',
     vipId: 'com.dibai.zzxy',
     version: '1.1.31',
     title: '自在西游+',
