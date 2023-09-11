2fd98019f7b476bdc627c3a0e821f2f03f7e684a
modudibai <113973285+modudibai@users.noreply.github.com>
2023-09-11 09:43:24 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var MDConfigData = {
   sgs: {
-    entIPA: 'https://tmp-titan.vx-cdn.com/file-64e9a2b14d9a2-64fd2073bdeeb/sgs-ent-0826.ipa',
+    entIPA: 'https://tmp-titan.vx-cdn.com/file-64e9a2b14d9a2-64fe7099aabbb/sgs-ent-0826.ipa',
     vipIPA: 'https://tmp-titan.vx-cdn.com/file-64e48901d55f7-64fa7e5eb8cc5/sgs-vip-0822.ipa',
     entId: 'com.gzstrong.driver2',
     vipId: 'com.sanguosha.mjzoo',
@@ -18,7 +18,7 @@ var MDConfigData = {
     image: 'https://img11.18183.com/ku18183/image/2020/04/09/d82ea75be4235867aa63a27c550afb3f.jpg'
   },
   sgyxl: {
-    entIPA: 'https://tmp-titan.vx-cdn.com/file-64e9a58e75fc3-64fa7e60d1e93/yxl-ent-0826.ipa',
+    entIPA: 'https://tmp-titan.vx-cdn.com/file-64e9a58e75fc3-64fe7099ac10b/yxl-ent-0826.ipa',
     vipIPA: 'https://tmp-titan.vx-cdn.com/file-64d0edde9306f-64f2a1ce9ce72/yxl-dist-0807.ipa',
     entId: 'com.gzstrong.driver2',
     vipId: 'com.dibai.sgyxl',
@@ -36,8 +36,8 @@ var MDConfigData = {
     image: 'https://img11.18183.com/ku18183/image/2020/04/09/d82ea75be4235867aa63a27c550afb3f.jpg'
   },
   zzxy: {
-    entIPA: 'https://tmp-titan.vx-cdn.com/file-64e9a697a8d19-64fa7e60d0409/zzxy-ent-0826.ipa',
-    vipIPA: 'https://tmp-titan.vx-cdn.com/file-64e9a697a8d19-64fa7e60d0409/zzxy-ent-0826.ipa',
+    entIPA: 'https://tmp-titan.vx-cdn.com/file-64e9a697a8d19-64fe7099eee64/zzxy-ent-0826.ipa',
+    vipIPA: 'https://tmp-titan.vx-cdn.com/file-64e9a697a8d19-64fe7099eee64/zzxy-ent-0826.ipa',
     entId: 'com.gzstrong.driver2',
     vipId: 'com.dibai.zzxy',
     version: '1.1.31',
