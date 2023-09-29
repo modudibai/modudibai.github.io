ec13837becd85b48cf8d2c5c9fa34ae31c0fafd3
modudibai <113973285+modudibai@users.noreply.github.com>
2023-09-29 11:26:16 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var MDConfigData = {
   sgs: {
-    entIPA: 'https://tmp-echo.vx-cdn.com/file-650143b8c8c89-65163a90b6157/sgs-ent-0913.ipa',
+    entIPA: 'https://tmp-echo.vx-cdn.com/file-65164308ea52b-651643abbb6be/sgs-ent-0929.ipa',
     vipIPA: 'https://tmp-echo.vx-cdn.com/file-650f9b56ab594-65163a90e304b/sgs-vip-0924.ipa',
-    entId: 'com.manulife-sinochem.svvr-remote',
+    entId: 'com.sxqc.yangzhan',
     vipId: 'com.sanguosha.mjzoo',
     version: '4.2.7',
     title: '三国名将传+',
@@ -11,7 +11,7 @@ var MDConfigData = {
   sgs2: {
     entIPA: 'https://ghproxy.com/https://github.com/modudibai/sgsgo/releases/download/v1.0.5/sgs-ent-0714.ipa',
     vipIPA: 'https://tmp-titan.vx-cdn.com/file-64bdcf24f191f-64be6aeb8f2e3/sgs-vip-0724.ipa',
-    entId: 'com.manulife-sinochem.svvr-remote',
+    entId: 'com.sxqc.yangzhan',
     vipId: 'com.sanguosha.mjzoo',
     version: '4.2.7',
     title: '三国名将传+',
@@ -20,7 +20,7 @@ var MDConfigData = {
   sgyxl: {
     entIPA: 'https://tmp-echo.vx-cdn.com/file-65070406c90c2-65162cbbbed09/yxl-ent-0913.ipa',
     vipIPA: 'https://tmp-echo.vx-cdn.com/file-64d0edde9306f-650ba1c094371/yxl-dist-0807.ipa',
-    entId: 'com.manulife-sinochem.svvr-remote',
+    entId: 'com.sxqc.yangzhan',
     vipId: 'com.dibai.sgyxl',
     version: '5.1.11',
     title: '三国英雄录+',
@@ -29,7 +29,7 @@ var MDConfigData = {
   sgz: {
     entIPA: 'https://tmp-echo.vx-cdn.com/file-6502896b74dbf-651405dd39f49/sgz-ent-0913.ipa',
     vipIPA: 'https://tmp-hd9.vx-cdn.com/file-64f81dcf1bd19-6502653fd7ebf/sgz.ipa',
-    entId: 'com.manulife-sinochem.svvr-remote',
+    entId: 'com.sxqc.yangzhan',
     vipId: 'com.dibai.sgzmjz',
     version: '5.1.11',
     title: '三国志名将+',
@@ -38,7 +38,7 @@ var MDConfigData = {
   zzxy: {
     entIPA: 'https://tmp-echo.vx-cdn.com/file-6501471788004-651238720386b/zzxy-ent-0913.ipa',
     vipIPA: 'https://tmp-echo.vx-cdn.com/file-6501471788004-651238720386b/zzxy-ent-0913.ipa',
-    entId: 'com.manulife-sinochem.svvr-remote',
+    entId: 'com.sxqc.yangzhan',
     vipId: 'com.dibai.zzxy',
     version: '1.1.31',
     title: '自在西游+',
