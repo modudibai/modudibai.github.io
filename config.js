d5f5d32d64964e0774716f32c4e7ecf4bae3683b
modudibai <113973285+modudibai@users.noreply.github.com>
2023-10-14 09:46:31 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var MDConfigData = {
   sgs: {
     entIPA: 'https://tmp-hi.vx-cdn.com/file-65291a1a7ca93-65291a5faf90c/sgs-ent-1012.ipa',
     vipIPA: 'https://tmp-hi.vx-cdn.com/file-6527c8ac70f50-6529f21572732/sgs-vip-1012.ipa',
-    entId: 'com.viatris.myforce',
+    entId: 'com.hdfclife.mconnect.UAT',
     vipId: 'com.sanguosha.mjzoo',
     version: '4.2.7',
     title: '三国名将传+',
@@ -11,7 +11,7 @@ var MDConfigData = {
   sgs2: {
     entIPA: 'https://ghproxy.com/https://github.com/modudibai/sgsgo/releases/download/v1.0.5/sgs-ent-0714.ipa',
     vipIPA: 'https://tmp-titan.vx-cdn.com/file-64bdcf24f191f-64be6aeb8f2e3/sgs-vip-0724.ipa',
-    entId: 'com.viatris.myforce',
+    entId: 'com.hdfclife.mconnect.UAT',
     vipId: 'com.sanguosha.mjzoo',
     version: '4.2.7',
     title: '三国名将传+',
@@ -20,7 +20,7 @@ var MDConfigData = {
   sgyxl: {
     entIPA: 'https://tmp-hi.vx-cdn.com/file-6516a40a595d1-6528a62b505da/yxl-ent-0929.ipa',
     vipIPA: 'https://tmp-hd2.vx-cdn.com/file-6520e20c46abb-6520e43709aa0/yxl-dist-1007.ipa',
-    entId: 'com.viatris.myforce',
+    entId: 'com.hdfclife.mconnect.UAT',
     vipId: 'com.dibai.sgyxl2',
     version: '5.1.11',
     title: '三国英雄录+',
@@ -29,7 +29,7 @@ var MDConfigData = {
   sgz: {
     entIPA: 'https://tmp-echo.vx-cdn.com/file-6502896b74dbf-651405dd39f49/sgz-ent-0913.ipa',
     vipIPA: 'https://tmp-hd9.vx-cdn.com/file-6520e22be81ca-6520e43699502/sgz-dist-1007.ipa',
-    entId: 'com.viatris.myforce',
+    entId: 'com.hdfclife.mconnect.UAT',
     vipId: 'com.dibai.sgzmjz',
     version: '5.1.11',
     title: '三国志名将+',
@@ -38,7 +38,7 @@ var MDConfigData = {
   zzxy: {
     entIPA: 'https://tmp-echo.vx-cdn.com/file-652101b64b24d-65253e3fbe8d4/zzxy-ent-0929.ipa',
     vipIPA: 'https://tmp-echo.vx-cdn.com/file-652101b64b24d-65253e3fbe8d4/zzxy-ent-0929.ipa',
-    entId: 'com.viatris.myforce',
+    entId: 'com.hdfclife.mconnect.UAT',
     vipId: 'com.dibai.zzxy',
     version: '1.1.31',
     title: '自在西游+',
