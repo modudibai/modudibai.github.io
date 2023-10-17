046738f0e2a02da6892ac49e045c50cea0c96372
modudibai <113973285+modudibai@users.noreply.github.com>
2023-10-17 09:44:38 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var MDConfigData = {
   sgs: {
-    entIPA: 'https://tmp-hd3.vx-cdn.com/file-6529f5efe13ab-652d0309aad54/sgs-ent-1014.ipa',
-    vipIPA: 'https://tmp-hd7.vx-cdn.com/file-6527c8ac70f50-652d0309a6891/sgs-vip-1012.ipa',
+    entIPA: 'https://tmp-hd3.vx-cdn.com/file-6529f5efe13ab-652de6cd8cab7/sgs-ent-1014.ipa',
+    vipIPA: 'https://tmp-hd7.vx-cdn.com/file-6527c8ac70f50-652de6d8dbb78/sgs-vip-1012.ipa',
     entId: 'com.hdfclife.mconnect.UAT',
     vipId: 'com.sanguosha.mjzoo',
     version: '4.2.7',
@@ -18,7 +18,7 @@ var MDConfigData = {
     image: 'https://img11.18183.com/ku18183/image/2020/04/09/d82ea75be4235867aa63a27c550afb3f.jpg'
   },
   sgyxl: {
-    entIPA: 'https://tmp-hd2.vx-cdn.com/file-6529fc2c23436-652d0309ae452/yxl-ent-1014.ipa',
+    entIPA: 'https://tmp-hd2.vx-cdn.com/file-6529fc2c23436-652de6cd87597/yxl-ent-1014.ipa',
     vipIPA: 'https://tmp-hd2.vx-cdn.com/file-6520e20c46abb-6520e43709aa0/yxl-dist-1007.ipa',
     entId: 'com.hdfclife.mconnect.UAT',
     vipId: 'com.dibai.sgyxl2',
@@ -36,8 +36,8 @@ var MDConfigData = {
     image: 'https://img11.18183.com/ku18183/image/2020/04/09/d82ea75be4235867aa63a27c550afb3f.jpg'
   },
   zzxy: {
-    entIPA: 'https://tmp-hd7.vx-cdn.com/file-652a0b0094a2f-652d030b91407/zzxy-ent-1014.ipa',
-    vipIPA: 'https://tmp-hd7.vx-cdn.com/file-652a0b0094a2f-652d030b91407/zzxy-ent-1014.ipa',
+    entIPA: 'https://tmp-hd7.vx-cdn.com/file-652a0b0094a2f-652de6cd8e41f/zzxy-ent-1014.ipa',
+    vipIPA: 'https://tmp-hd7.vx-cdn.com/file-652a0b0094a2f-652de6cd8e41f/zzxy-ent-1014.ipa',
     entId: 'com.hdfclife.mconnect.UAT',
     vipId: 'com.dibai.zzxy',
     version: '1.1.31',
