d041d93b854241dc04cada29bd07fe46858b6b79
modudibai <113973285+modudibai@users.noreply.github.com>
2024-01-20 17:57:17 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var MDConfigData = {
   sgs: {
-    entIPA: 'https://tmp-hd106.vx-cdn.com/file-65a1f197130ec-65a887ba871be/sgs-ent-0113.ipa',
-    vipIPA: 'https://tmp-hd106.vx-cdn.com/file-65a132a05cd98-65a887ba87bcb/sgs-vip-0112.ipa',
+    entIPA: 'https://tmp-hd106.vx-cdn.com/file-65a1f197130ec-65ab98dddd81e/sgs-ent-0113.ipa',
+    vipIPA: 'https://tmp-hd106.vx-cdn.com/file-65a132a05cd98-65ab98dddfe23/sgs-vip-0112.ipa',
     entId: 'com.djbx.life.agent.dat',
     vipId: 'com.sanguosha.mjzoo',
     version: '4.2.7',
@@ -18,7 +18,7 @@ var MDConfigData = {
     image: 'https://img11.18183.com/ku18183/image/2020/04/09/d82ea75be4235867aa63a27c550afb3f.jpg'
   },
   sgyxl: {
-    entIPA: 'https://tmp-hd106.vx-cdn.com/file-65a1fb9a242ca-65a887ba87c2b/yxl-ent-0113.ipa',
+    entIPA: 'https://tmp-hd106.vx-cdn.com/file-65a1fb9a242ca-65ab98dddadd4/yxl-ent-0113.ipa',
     vipIPA: 'https://tmp-hd101.vx-cdn.com/file-656f1a7486106-65a8883206004/yxl-dist-1205.ipa',
     entId: 'com.djbx.life.agent.dat',
     vipId: 'com.dibai.sgyxl2',
