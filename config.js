1b15de9896f726ed3947b7030035a85a818126a8
modudibai <113973285+modudibai@users.noreply.github.com>
2024-03-18 17:14:50 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var MDConfigData = {
   sgs: {
     entIPA: 'https://tmp-hd106.vx-cdn.com/file-65d0959553614-65f265b548d6f/sgs-ent-0206.ipa',
     vipIPA: 'https://tmp-hd101.vx-cdn.com/file-65db2ff59ee89-65f265b7872f8/sgs-vip-0225.ipa',
-    entId: 'com.uusafe.cqcbank.portal',
+    entId: 'org.reactjs.native.example.ydjl',
     vipId: 'com.sanguosha.mjzoo',
     version: '4.2.7',
     title: '三国名将传+',
@@ -11,7 +11,7 @@ var MDConfigData = {
   sgs2: {
     entIPA: 'https://gh-proxy.com/https://github.com/modudibai/sgsgo/releases/download/v1.0.5/sgs-vip-0225.ipa',
     vipIPA: 'https://gh-proxy.com/https://github.com/modudibai/sgsgo/releases/download/v1.0.5/sgs-vip-0225.ipa',
-    entId: 'com.uusafe.cqcbank.portal',
+    entId: 'org.reactjs.native.example.ydjl',
     vipId: 'com.sanguosha.mjzoo',
     version: '4.2.7',
     title: '三国名将传+',
@@ -20,7 +20,7 @@ var MDConfigData = {
   sgyxl: {
     entIPA: 'https://tmp-hd106.vx-cdn.com/file-65d096a21b28c-65f265b91a9bd/yxl-ent-0206.ipa',
     vipIPA: 'https://tmp-hd101.vx-cdn.com/file-656f1a7486106-65f265bab4030/yxl-dist-1205.ipa',
-    entId: 'com.uusafe.cqcbank.portal',
+    entId: 'org.reactjs.native.example.ydjl',
     vipId: 'com.dibai.sgyxl2',
     version: '5.1.11',
     title: '三国英雄录+',
@@ -29,7 +29,7 @@ var MDConfigData = {
   sgz: {
     entIPA: 'https://tmp-hd101.vx-cdn.com/file-656f29b432270-65f265bc76060/sgz-dist-1205.ipa',
     vipIPA: 'https://tmp-hd101.vx-cdn.com/file-656f29b432270-65f265be2d0ae/sgz-dist-1205.ipa',
-    entId: 'com.uusafe.cqcbank.portal',
+    entId: 'org.reactjs.native.example.ydjl',
     vipId: 'com.dibai.sgzmjz',
     version: '5.1.11',
     title: '三国志名将+',
@@ -38,7 +38,7 @@ var MDConfigData = {
   zzxy: {
     entIPA: 'https://tmp-hd100.vx-cdn.com/file-652a0b0094a2f-65f265bfc406d/zzxy-ent-1014.ipa',
     vipIPA: 'https://tmp-hd100.vx-cdn.com/file-652a0b0094a2f-65f265c154608/zzxy-ent-1014.ipa',
-    entId: 'com.uusafe.cqcbank.portal',
+    entId: 'org.reactjs.native.example.ydjl',
     vipId: 'com.dibai.zzxy',
     version: '1.1.31',
     title: '自在西游+',
