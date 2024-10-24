2b3d5a86cb5e0fe43e542fa240c3f52df619851f
modudibai <113973285+modudibai@users.noreply.github.com>
2024-10-24 18:08:33 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var MDConfigData = {
   sgs: {
-    entIPA: 'https://cache.5t-cdn.com/file-670fabd47bfdc-671922cc2ffa8/sgs-vip-1016.ipa',
-    vipIPA: 'https://cache.5t-cdn.com/file-670fabd47bfdc-671922cc2ffa8/sgs-vip-1016.ipa',
+    entIPA: 'https://cache.5t-cdn.com/file-670fabd47bfdc-671a1c94cd01f/sgs-vip-1016.ipa',
+    vipIPA: 'https://cache.5t-cdn.com/file-670fabd47bfdc-671a1c94cd01f/sgs-vip-1016.ipa',
     entId: 'com.sanguosha.mjzoo',
     vipId: 'com.sanguosha.mjzoo',
     version: '4.2.7',
